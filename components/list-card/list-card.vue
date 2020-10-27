f15d5c8440ad98f761e24f918d7c821472dd2c9a
Lc <641229781@qq.com>
2020-10-27 11:05:30 +0800

    首页三种内容布局样式完成

--- components/list-card/list-card.vue
+++ components/list-card/list-card.vue
@@ -1,10 +1,60 @@
 <template>
 	<view>
 		<!-- 基础卡片 -->
-		<view class="listcard">
+		<view v-if="mode === 'base'" class="listcard">
 			<view class="listcard-image">
 				<image src="../../static/logo.png" mode="aspectFill"></image>
 			</view>
+			<view class="listcard-content">
+				<view class="listcard-content_title">
+					<text>
+						uni-app开发框架uni-app开发框架uni-app开发框架uni-app开发框架uni-app开发框架uni-app开发框架uni-app开发框架uni-app开发框架uni-app开发框架uni-app开发框架uni-app开发框架uni-app开发框架
+					</text>
+				</view>
+				<view class="listcard-content_des">
+					<view class="listcard-content_desc-label">
+						<view class="listcard-content_desc-label-item">
+							前端
+						</view>
+					</view>
+					<view class="listcard-content_desc-browse">120浏览</view>
+				</view>
+			</view>
+		</view>
+		
+		
+		<!-- 多图卡片 -->
+		<view v-if="mode === 'column'" class="listcard mode-column">
+			<view class="listcard-content">
+				<view class="listcard-content_title">
+					<text>
+						uni-app开发框架uni-app开发框架uni-app开发框架uni-app开发框架uni-app开发框架uni-app开发框架uni-app开发框架uni-app开发框架uni-app开发框架uni-app开发框架uni-app开发框架uni-app开发框架
+					</text>
+				</view>
+				<view class="listcard-image">
+					<view v-for="item in 3" :key="item" class="listcard-image_item">
+						<image src="../../static/logo.png" mode="aspectFill"></image>
+					</view>
+				</view>
+				
+				<view class="listcard-content_des">
+					<view class="listcard-content_desc-label">
+						<view class="listcard-content_desc-label-item">
+							前端
+						</view>
+					</view>
+					<view class="listcard-content_desc-browse">120浏览</view>
+				</view>
+			</view>
+		</view>
+		
+		
+		<!-- 大图卡片 -->
+		<view v-if="mode === 'image'" class="listcard mode-image">
+			<view class="listcard-image">
+				<image src="../../static/logo.png" mode="aspectFill"></image>
+			</view>
+			
 			<view class="listcard-content">
 				<view class="listcard-content_title">
 					<text>
@@ -26,6 +76,12 @@
 
 <script>
 	export default {
+		props:{
+			mode: {
+				type:String,
+				default: 'base'
+			}
+		},
 		data() {
 			return {
 				
@@ -51,13 +107,13 @@
 		image {
 			width: 100%;
 			height: 100%;
-		}
+		}	
 	}
 	.listcard-content {
 		display: flex;
 		flex-direction: column;
 		justify-content: space-between;
-		margin-left: 20upx;
+		padding-left: 20upx;
 		width: 100%;
 		.listcard-content_title {
 			font-size: 28upx;
@@ -91,6 +147,55 @@
 				line-height: 1.2;
 			}
 		}
+	}
+	
+	// 多图模式的样式
+	&.mode-column {
+		.listcard-content {
+			width: 100%;
+			padding-left: 0;
+		}
+		.listcard-image {
+			display: flex;
+			margin-top: 20upx;
+			width: 100%;
+			height: 140upx;
+			.listcard-image_item {
+				margin-left: 20upx;
+				width: 100%;
+				border-radius: 10upx;
+				overflow: hidden;
+				&:first-child {
+					margin-left: 0;
+				}
+				image {
+					width: 100%;
+					height: 100%;
+				}
+			}
+		}
+		.listcard-content_des {
+			margin-top: 20upx;
+		}
+	}
+	
+	// 大图模式
+	&.mode-image {
+		flex-direction: column;
+		.listcard-image {
+			width: 100%;
+			height: 200upx;
+		}
+		.listcard-content {
+			padding-left: 0;
+			margin-top: 20upx;
+			.listcard-content_des {
+				display: flex;
+				align-items: center;
+				margin-top: 20upx;
+			}
+		}
+		
 	}
 }
 </style>

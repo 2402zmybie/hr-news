11e59a5554c0487e15bbc5dd9e04a58f879cb543
Lc <641229781@qq.com>
2020-10-30 10:47:13 +0800

    实现标签筛选的云函数

--- cloudfunctions-aliyun/get_label/index.js
+++ cloudfunctions-aliyun/get_label/index.js
@@ -1,11 +1,37 @@
 'use strict';
 //获取数据库的引用
 const db = uniCloud.database()
+//获得 聚合操作符
+const $ = db.command.aggregate
 exports.main = async (event, context) => {
 	//event为客户端上传的参数
 	console.log('event : ', event)
 	
-	let label = await db.collection("label").get()
+	const {
+		user_id,
+		type
+	} = event
+	
+	let matchObj = {}
+	if(type !== 'all') {
+		//只返回 我的标签
+		matchObj = {
+			current: true
+		}
+	}
+	let userinfo = await db.collection('user').doc(user_id).get()
+	userinfo = userinfo.data[0]
+	
+	// let label = await db.collection("label").get()
+	
+	let label = await db.collection("label")
+	.aggregate()
+	.addFields({
+		//current 是true,表示是我的标签, false则表示是其他标签
+		current: $.in(['$_id', $.ifNull([userinfo.label_ids,[]])])
+	})
+	.match(matchObj)
+	.end()
 	
 	
 	
---
+++ pages/home-label/home-label.vue
@@ -0,0 +1,81 @@
+<template>
+	<view class="label">
+		<view class="label-box">
+			<view class="label-header">
+				<view class="label-title">我的标签</view>
+				<view class="label-edit">编辑</view>
+			</view>
+			<view class="label-content">
+				<view class="label-content_item" v-for="item in 10">
+					{{item}}标签
+					<uni-icons class="icons-close" type="clear" size="16" color="red"></uni-icons>
+				</view>
+			</view>
+		</view>
+		<view class="label-box">
+			<view class="label-header">
+				<view class="label-title">标签推荐</view>
+			</view>
+			<view class="label-content">
+				<view class="label-content_item" v-for="item in 10">{{item}}标签</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+page {
+	background-color: #f5f5f5;
+}
+.label {
+	.label-box {
+		background-color: #fff;
+		margin-bottom: 20upx;
+		.label-header {
+			padding: 20upx 30upx;
+			border-bottom: 2upx solid #f5f5f5;
+			display: flex;
+			justify-content: space-between;
+			font-size: 28upx;
+			color: #666;
+			.label-edit {
+				color: #30b33a;
+				font-weight: bold;
+			}
+		}
+		.label-content {
+			display: flex;
+			flex-wrap: wrap;
+			padding: 30upx;
+			padding-top: 0;
+			.label-content_item {
+				position: relative;
+				padding: 4upx 10upx;
+				margin-top: 24upx;
+				margin-right: 20upx;
+				border-radius: 10upx;
+				border: 2upx solid #666;
+				font-size: 28upx;
+				color: #666;
+				.icons-close {
+					position: absolute;
+					right: -16upx;
+					top: -16upx;
+					background-color: #fff;
+					border-radius: 50%;
+				}
+			}
+		}
+	}
+}
+</style>
